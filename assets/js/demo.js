bf6df56dc9609034eea8e5a9bcb1a0b59c7066b8
Muhammad Mujtaba <twosep89@gmail.com>
2020-09-01 15:58:57 +0400

    update js demo

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -16,13 +16,13 @@ $.contactButtons({
   effect  : 'slide-on-scroll',
   buttons : {
     'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/muj989', extras: 'target="_blank"' },
-	'twitter':   { class: 'twitter', use: true, link: '#', extras: 'target="_blank"' },
+	'twitter':   { class: 'twitter', use: true, link: 'https://https://twitter.com/twosep89', extras: 'target="_blank"' },
 	'instagram':   { class: 'instagram', use: true, link: 'https://www.instagram.com/owais989/', extras: 'target="_blank"' },
     'linkedin':   { class: 'linkedin', use: true, link: 'https://www.linkedin.com/in/mujtaba989/', extras: 'target="_blank"' },
 	'pinterest':     { class: 'pinterest',    use: true, link: 'https://www.pinterest.com/mujtabakamal123/ss' },
 	'youtube':     { class: 'youtube',    use: true, link: 'https://www.pinterest.com/mujtabakamal123/ss' },
-    'whatsapp':     { class: 'whatsapp',    use: true, link: 'https://api.whatsapp.com/send?phone=971552466432&text=Hi, I contacted you Through your website.' },
-    'phone':      { class: 'phone separated',    use: true, link: '+971552466432' },
+    'whatsapp':     { class: 'whatsapp',    use: true, link: 'https://api.whatsapp.com/send?phone=923463510531&text=Hi, I contacted you Through your website.' },
+    'phone':      { class: 'phone separated',    use: true, link: '+923463510531' },
     'email':      { class: 'email',    use: true, link: 'twosep89@gmail.com' }
   }
 });
